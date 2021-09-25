267e0eb27cc4d8901d0f5dfbca1af29c14c3c748
u9rihara <kurihara@uzu-lab.co.jp>
2021-09-25 18:05:20 +0900

    ダイアログのスタイル調整＆背景クリックで閉じるように

--- src/modules/dialog.js
+++ src/modules/dialog.js
@@ -8,20 +8,29 @@ export function dialog(events) {
   dialog.setAttribute('id', 'irToolDialog');
   document.body.appendChild(dialog);
 
-  let innerDialog = '<h2 class="dialog_title">IR案件補助ツール</h2><ul class="tools">';
+  let innerDialog = '<div class="dialog_inner"><div class="dialog_title">IR案件補助ツール</div><ul class="tools">';
   events.forEach(event => {
     innerDialog += `
       <li class="tool"><button class="tool_button" id="irTool${event.id}">${event.label}</button></li>
     `;
   });
-  innerDialog += '</ul><p class="dialog_close"><button class="close_button" id="closeIrToolDialog">閉じる</button></p>';
+  innerDialog += '</ul><p class="dialog_close"><button class="close_button" id="closeIrToolDialog">閉じる</button></p></div>';
   dialog.innerHTML = innerDialog;
 
   events.forEach(event => {
     document.getElementById('irTool' + event.id).addEventListener('click', event.callback);
   });
+
+  // Close
+  dialog.addEventListener('click', () => {
+    dialog.close();
+  }, false);
   document.getElementById('closeIrToolDialog').addEventListener('click', () => {
     dialog.close();
   });
+  dialog.querySelector('.dialog_inner').addEventListener('click', (e) => {
+    e.stopPropagation();
+  }, false);
+
   dialog.showModal();
 }
