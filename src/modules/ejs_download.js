8f258b29231c0513361d5b57011fa8fe4295046f
u9rihara <kurihara@uzu-lab.co.jp>
2021-09-28 19:39:28 +0900

    サイト内リンクをローカル環境で開けるように書き換え

--- src/modules/ejs_download.js
+++ src/modules/ejs_download.js
@@ -20,9 +20,32 @@ export function ejs_download() {
    * CSS/JSの挿入。挿入済みのコードは削除する。
    */
   const insertHtml = (html) => {
-    const ins_html = `<link href="/assets/css/custom.css" rel="stylesheet"><script src="/assets/js/custom.js"></script>`;
-    html = html.replace(/(<link.+?>)?(<script.+?>)?<\/head>/g, ins_html + '</head>');
+    const hdr_ins_html = `<link href="/assets/css/custom.css" rel="stylesheet"><script src="/assets/js/custom.js"></script>`;
+    html = html.replace(/(<link.+?>)?(<script.+?>)?<\/head>/g, hdr_ins_html + '</head>');
+    const ftr_ins_html = `<script>
+      jQuery(function($){
+        $('a[href^="/"]').attr('href', function(i, v){
+          var local_href = '/';
+          if (v !== local_href) {
+            local_href = v.replace(/^(.+?)\/?(#.+)?$/, '$1.html$2');
+          }
+          return local_href;
+        });
+      });
+    </script>`;
+    html = html.replace(/<\/body>/g, ftr_ins_html + '</body>');
     return html;
+  };
+
+  /**
+   * 内部リンクをローカル化
+   */
+  const localizeHref = (html) => {
+    const domain = window.location.hostname;
+    const regexp1 = new RegExp('<a(.+?)href="http(s)?:\/\/' + domain + '\/?"', 'g');
+    html = html.replace(regexp1, '<a$1href="/"'); // トップへのリンク
+    const regexp2 = new RegExp('<a(.+?)href="http(s)?:\/\/' + domain + '(.*?)\/?"', 'g');
+    return html.replace(regexp2, '<a$1href="$3"'); // 下層へのリンク
   };
 
   /**
@@ -34,7 +57,6 @@ export function ejs_download() {
     link.href = URL.createObjectURL(blob);
     let filename = window.location.pathname.replace(/(.+)\/$/, '$1').split('/').pop();
     filename = filename ? filename.replace(/(\..+)?$/, '.ejs') : 'index.ejs';
-    console.log(filename);
     link.download = filename;
     link.click();
   };
@@ -46,6 +68,7 @@ export function ejs_download() {
       fetchSelf()
         .then((text) => {
           text = insertHtml(text);
+          text = localizeHref(text)
           download(text);
         })
     }
